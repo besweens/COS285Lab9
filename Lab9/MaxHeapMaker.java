56554847982cfe0dfa494d46b930a6d59681da26
besweens <144698652+besweens@users.noreply.github.com>
2023-11-27 14:53:35 -0500

    Begin heapify, add RunningExample.java

--- Lab9/MaxHeapMaker.java
+++ Lab9/MaxHeapMaker.java
@@ -1,10 +1,11 @@
 package Lab9;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MaxHeapMaker {
     /**
-     * This method takes in an array of integers and returns a max-heap 
+     * This method takes in an array of integers and returns a max-heap
      * @param input array of integer values
      * @return input as a max-heap
      *
@@ -15,20 +16,27 @@ public class MaxHeapMaker {
     {
         int[]tempHeap = new int[input.length];
         int swap=0;
-        // tempHeap[0]=input[0];
-        for(int i=0; i< input.length;i++){
-            tempHeap[i]=input[i];
-            if (i>0){
-                for(int j=0; j< input.length;j++){
-                    if(tempHeap[i]>tempHeap[j]) {
-                        swap = tempHeap[i];
-                        tempHeap[i]=tempHeap[j];
-                        tempHeap[j]=swap;
-                    }
-                }
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for(int i=0; i< input.length;i++) {
+            tempList.addLast(input[i]);
+            if();
         }
 
-        //find max, first pos
+//        for(int i=0; i< input.length;i++){
+//            tempHeap[i]=input[i];
+//            if (i>0){
+//                for(int j=0; j< input.length;j++){
+//                    if(tempHeap[i]>tempHeap[j]) {
+//                        swap = tempHeap[i];
+//                        tempHeap[i]=tempHeap[j];
+//                        tempHeap[j]=swap;
+//                    }
+//                }
+//        }
+/*
+insert parent
+insert child at parentpos*2 , parentpos*2 + ;
+ */
         return input;
     }
 }
